a38ccc2322c102f4cf864662039026b96c571f22
Juan Veron <juan_juve97@hotmail.com>
2022-07-13 20:06:13 -0300

    se logró poner los botones en las cards y cambio de fondo

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,21 +1,46 @@
 import "../styles/Cards.css"
+import { useState } from "react";
+
 
 export default function Cards(props){
+        const [play, setPlay] = useState(false);
+
+
+        const hoverPlay = () => {
+                setPlay(!play);
+                
+            }
+
+
+
 
         const fondo = document.querySelector('.home-container');
         const front = document.querySelector('.main-container--grid_item');
+        const button = document.querySelector('.inactive');
 
+        
         
         const aver = ()=>{
                 fondo.classList.toggle('back');
-              }
+                
+        }
+        const verplay = ()=>{
+                button.classList.toggle('inactive');
+        }
+
 
         return(
             <div
-            onMouseEnter={aver} 
-                onMouseLeave={aver}
-            className='main-container--grid_item'>
-                    <img src={props.image} ></img>
+            
+            onMouseEnter={(event)=> {hoverPlay(event), aver(event)} }
+                onMouseLeave={(event)=> {hoverPlay(event), aver(event)}} 
+                
+            className='main-container--grid_item' >
+                <img  src={props.image} ></img>
+                {play && <span  class="material-symbols-outlined">
+                        play_circle
+                </span>}
+                
                     <h3></h3>
                     <p></p>
             </div>
--- src/components/templatesPlay/Changues.jsx
+++ src/components/templatesPlay/Changues.jsx
@@ -6,9 +6,12 @@ export default function Changues(){
         <TemplateSongs
         title='Justin bieber - Forever'
         image='https://e00-marca.uecdn.es/assets/multimedia/imagenes/2020/02/14/15816909157779.jpg'
-        disco='https://i.pinimg.com/564x/25/86/c0/2586c0d60768e1952326ee585f193332.jpg'
+        disco='https://pbs.twimg.com/media/EQv8g1LWAAsCukn.jpg:large'
         />
     )
-}
+ }
+
+
+// https://i.pinimg.com/564x/25/86/c0/2586c0d60768e1952326ee585f193332.jpg
 
 
